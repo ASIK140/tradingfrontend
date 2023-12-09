49f2f4eed2563c815ce3f64027940f9a743762e8
Saikat <saikat2@gmail.com>
2023-12-09 19:59:42 +0530

    proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://tradingadda.onrender.com",
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   plugins: [react()],
